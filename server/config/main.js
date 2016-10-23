cdd2fcf9d6ac5aa055a666cc0b7547916ff2d89e
davdkm <davd.km@gmail.com>
2016-10-22 23:07:04 -0700

    Add mongoose to project

--- server/config/main.js
+++ server/config/main.js
@@ -1,3 +1,8 @@
 module.exports = {
+  // Secret key for JWT signing and encryption
+  'secret': 'battlerite base is the bess',
+  // Database connection information
+  'database': 'mongodb://localhost:27017',
+  // Setting port of server
   'port': process.env.PORT || 3000
 }
